85e33d0168f09b31dd870db24908c98cf041b7b7
Brianna O'Keefe <bri@songoda.com>
2024-03-31 15:43:45 -0500

    Added a menu command.

---
+++ src/main/java/com/songoda/ultimaterifts/commands/CommandMenu.java
@@ -0,0 +1,61 @@
+package com.songoda.ultimaterifts.commands;
+
+import com.craftaro.core.commands.AbstractCommand;
+import com.craftaro.third_party.com.cryptomorin.xseries.XSound;
+import com.songoda.ultimaterifts.UltimateRifts;
+import com.songoda.ultimaterifts.rift.Rift;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+public class CommandMenu extends AbstractCommand {
+    private final UltimateRifts plugin;
+
+    public CommandMenu(UltimateRifts plugin) {
+        super(CommandType.PLAYER_ONLY, "menu");
+        this.plugin = plugin;
+    }
+
+    @Override
+    protected ReturnType runCommand(CommandSender sender, String... args) {
+        Player player = (Player) sender;
+        Rift rift = plugin.getRiftManager().getRiftByLocation(player.getLocation());
+        if (rift == null) {
+            plugin.getLocale().getMessage("command.info.notinrift").sendPrefixedMessage(sender);
+            XSound.ENTITY_VILLAGER_NO.play(player);
+            return ReturnType.FAILURE;
+        }
+
+        if (!rift.hasAccess(player)) {
+            plugin.getLocale().getMessage("event.interact.notowner").sendPrefixedMessage(player);
+            XSound.ENTITY_VILLAGER_NO.play(player);
+            return ReturnType.FAILURE;
+        }
+
+        // Open the rift menu for the player
+        plugin.getGuiManager().showGUI(player, rift.getOverviewGui(player));
+
+        return ReturnType.SUCCESS;
+    }
+
+    @Override
+    protected List<String> onTab(CommandSender sender, String... args) {
+        return null;
+    }
+
+    @Override
+    public String getPermissionNode() {
+        return "ultimaterifts.menu";
+    }
+
+    @Override
+    public String getSyntax() {
+        return "menu";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Opens the rift menu for the current rift.";
+    }
+}
